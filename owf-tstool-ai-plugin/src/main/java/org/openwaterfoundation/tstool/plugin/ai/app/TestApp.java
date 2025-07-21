b948062174931df058a7da2e073470184394776c
Steve Malers <steve.malers@openwaterfoundation.org>
2025-07-21 13:21:23 -0600

    Initialize the plugin command, issue #6

---
+++ owf-tstool-ai-plugin/src/main/java/org/openwaterfoundation/tstool/plugin/ai/app/TestApp.java
@@ -0,0 +1,13 @@
+package org.openwaterfoundation.tstool.plugin.ai.app;
+
+/**
+ * Create application classes in this package.
+ */
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
